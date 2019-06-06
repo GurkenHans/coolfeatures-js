62b1b174b6af79259cada9dd09aa0e5c8c03955c
GurkenHans <nicoboerger63@gmail.com>
2019-06-06 14:52:23 +0200

    Added filterObject method

--- coolfeatures.js
+++ coolfeatures.js
@@ -4,6 +4,17 @@ var Coolfeatures = {
 
         return array[this.getRandomIntBetween(0, array.length - 1)];
     },
+    filterObject: function(object, callback) {
+        const newObject = {};
+
+        for(let key in object) {
+            if(callback(object[key], key, object)) {
+                newObject[key] = object[key];
+            }
+        }
+        
+        return newObject;
+    },
     getRandomIntBetween: function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
