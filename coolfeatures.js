d3b1747bf708a04df1a505efe2c8da4a54bbbb68
GurkenHans <nicoboerger63@gmail.com>
2019-06-06 13:34:13 +0200

    Bugfix

--- coolfeatures.js
+++ coolfeatures.js
@@ -1,12 +1,10 @@
-function Coolfeatures() {
-    return {
-        getRandomArrayValue: function(array) {
-            array = array.filter((value, index) => index in array);
+const Coolfeatures = {
+    getRandomArrayValue: function(array) {
+        array = array.filter((value, index) => index in array);
 
-            return array[this.getRandomIntBetween(0, array.length - 1)];
-        },
-        getRandomIntBetween: function(from, to) {
-            return Math.floor(Math.random() * (to - from)) + from;
-        }
+        return array[this.getRandomIntBetween(0, array.length - 1)];
+    },
+    getRandomIntBetween: function(from, to) {
+        return Math.floor(Math.random() * (to - from)) + from;
     }
 }
